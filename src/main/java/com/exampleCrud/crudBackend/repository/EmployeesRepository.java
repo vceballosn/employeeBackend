0006c070948ae6b96f267a7f9ff60d410e8ae884
vceballosn <vladimir.ceballos@gmail.com>
2025-04-25 18:20:14 -0400

    add update api

--- src/main/java/com/exampleCrud/crudBackend/repository/EmployeesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.exampleCrud.crudBackend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.exampleCrud.crudBackend.entity.Employee;
-
-public interface EmployeesRepository   extends JpaRepository<Employee, Long>  {
-
-}
--- src/main/java/com/exampleCrud/crudBackend/service/impl/package-info.java
+++
@@ -1,1 +0,0 @@
-package com.exampleCrud.crudBackend.service.impl;
